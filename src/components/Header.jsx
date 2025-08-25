0f29dd73d6aee9ecdbbcc5979121a311813d0f6d
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-08-25 17:41:22 +0200

    feat: Header> html-structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <header>
+      <div id="logo">
+      <img src="" alt="" />
+      </div>
+      <div>
+        <nav>
+          <ul>
+            <li>CHARACTERS</li>
+            <li>COMICS</li>
+            <li>MOVIES</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEOS</li>
+            <li>FANS</li>
+            <li>NEWS</li>
+            <li>SHOP</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   )
 }
 
