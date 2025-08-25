e57a7ae3f4918118c3c9139a7e4955dfc2e91718
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-08-25 18:11:27 +0200

    feat: Footer> html-initial-content-structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,72 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-      <div id="shop-list"></div>
-      <div id="others"></div>
-      <div id="our-links"></div>
+      <div id="shop-list">
+        <ul>
+          <li>DIGITAL COMICS</li>
+          <li>DC MERCHANDISE</li>
+          <li>SUBSCRIPTION</li>
+          <li>COMIC SHOP LOCATOR</li>
+          <li>DC POWER VISA</li>
+        </ul>
+      </div>
+      <div id="others">
+        <div>
+          <h3></h3>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+          <h3></h3>
+          <ul>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        <div>
+          <h3></h3>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </div>
+        <div>
+          <h3></h3>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        </div>
+      <div id="our-links">
+        <div className="btn"></div>
+        <div>
+          <a href="">FOLLOW US</a>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
     </footer>
   )
 }
